5be8f7d4c053d31d47540e2472168b850846eceb
Michael Gibson Bethke <gibsonbethke@gmail.com>
2015-10-19 13:48:14 -0500

    Considering Addition of New Class to Engine

---
+++ OS X/Light-Year/Light-Year/CustomClassStructure.h
@@ -0,0 +1,14 @@
+//
+//  CustomClassStructure.h
+//  Light-Year
+//
+//  Created by Michael Bethke on 10/19/15.
+//  Copyright © 2015 2Cat Studios. All rights reserved.
+//
+
+#ifndef CustomClassStructure_h
+#define CustomClassStructure_h
+
+#include <stdio.h>
+
+#endif /* CustomClassStructure_h */
