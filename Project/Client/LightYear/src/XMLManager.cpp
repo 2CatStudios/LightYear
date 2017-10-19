04da56e3478d49bddc24171590937995380335a5
Michael Bethke <gibsonbethke@gmail.com>
2017-10-18 21:50:19 -0500

    Removed commented-out code

--- Project/Client/LightYear/src/XMLManager.cpp
+++ Project/Client/LightYear/src/XMLManager.cpp
@@ -2,8 +2,6 @@
 
 pugi::xml_parse_result XMLManager::ReadFromFileToDoc (const std::string filePath, pugi::xml_document &doc)
 {
-	
-	//pugi::xml_parse_result result = doc.load_file(filePath.c_str());
 		
 	return doc.load_file(filePath.c_str());
 }
